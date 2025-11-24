345a1b83d78db7f2fa720ffb24ca1e9c55fbdc88
ahmed04raxa <ahmed04raxa@gmail.com>
2025-11-24 11:58:22 +0500

    Project Set Up

---
+++ lib/domain/app_routes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:spotifyuiclone/ui/splash/splash_screen.dart';
+
+class AppRoutes {
+  static const String splashScreen = "/";
+  static const String introScreen = "/intro";
+
+  static Map<String, Widget Function(BuildContext)> getRoutes() => {
+    splashScreen: (context) => SplashScreen(),
+  };
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:spotifyuiclone/domain/app_routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Spotify',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        fontFamily: "regular",
+      ),
+      routes: AppRoutes.getRoutes(),
+      initialRoute: "/",
+    );
+  }
+}
